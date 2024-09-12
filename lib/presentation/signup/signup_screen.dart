b8b93664ce1b7231fd182189ae9e539729425937
Judy942 <109392364+Judy942@users.noreply.github.com>
2024-09-12 10:59:52 +0700

    .

--- lib/presentation/signup/signup_screen.dart
+++ lib/presentation/signup/signup_screen.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -21,42 +20,69 @@ class _SignupScreenState extends State<SignupScreen> {
   String password = "";
   String firstName = "";
   String lastName = "";
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController firstNameController = TextEditingController();
-  TextEditingController lastNameController = TextEditingController();
+  // TextEditingController emailController = TextEditingController();
+  // TextEditingController passwordController = TextEditingController();
+  // TextEditingController firstNameController = TextEditingController();
+  // TextEditingController lastNameController = TextEditingController();
 
+  void onRegisterButtonPressed() {
+    if (email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Email and password are required'),
+        ),
+      );
+      return;
+    } 
+    if(!email.contains('@')||!email.contains('.')||email.characters.last=='.'||email.characters.last=='@'||email.characters.first=='.'||email.characters.first=='@'){
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Invalid email'),
+        ),
+      );
+      return;
+    }
+    //password validation: gồm cả số , chữ hoa, chữ thường, ký tự đặc biệt
+    if(password.length<6||!password.contains(RegExp(r'[0-9]'))||!password.contains(RegExp(r'[A-Z]'))||!password.contains(RegExp(r'[a-z]'))||!password.contains(RegExp(r'[!@#%^&*(),.?":{}|<>]'))){
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Password must contain at least 6 characters, including uppercase, lowercase, number and special character'),
+        ),
+      );
+      return;
+    }
 
+    fetchData();
+  }
 
   void fetchData() async {
-  // var url = Uri.parse('http://162.248.102.236:8055/auth/login');
-  String url = "http://162.248.102.236:8055/users/register";
-  print("Email: $email");
-  print("Password: $password");
-  
+    // var url = Uri.parse('http://162.248.102.236:8055/auth/login');
+    String url = "http://162.248.102.236:8055/users/register";
+    print("Email: $email");
+    print("Password: $password");
 
-  final response = await http.post(
-    Uri.parse(url),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      "email": email,
-      "password": password,
-    }),
-  );
+    final response = await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        "email": email,
+        "password": password,
+      }),
+    );
     print(jsonDecode(response.body));
     print(response.statusCode);
     if (response.statusCode == 200) {
       Navigator.pushNamed(context, '/completeProfileScreen');
-    } else if(response.statusCode == 400) {
+    } else if (response.statusCode == 400) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Invalid email or password'),
         ),
       );
     }
-}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -93,10 +119,10 @@ class _SignupScreenState extends State<SignupScreen> {
                 const SizedBox(
                   height: 15,
                 ),
-                 RoundTextField(
+                RoundTextField(
                   onChanged: (value) {
                     setState(() {
-                      email = value;
+                      firstName = value;
                     });
                   },
                   hintText: "First Name",
@@ -106,18 +132,24 @@ class _SignupScreenState extends State<SignupScreen> {
                 const SizedBox(
                   height: 15,
                 ),
-                 RoundTextField(
-                  onChanged: (p0) {
-                  },
+                RoundTextField(
+                    onChanged: (p0) {
+                      setState(() {
+                        lastName = p0;
+                      });
+                    },
                     hintText: "Last Name",
                     icon: "assets/icons/profile_icon.png",
                     textInputType: TextInputType.name),
                 const SizedBox(
                   height: 15,
                 ),
-                 RoundTextField(
-                  onChanged: (p0) {
-                  },
+                RoundTextField(
+                    onChanged: (p0) {
+                      setState(() {
+                        email = p0;
+                      });
+                    },
                     hintText: "Email",
                     icon: "assets/icons/message_icon.png",
                     textInputType: TextInputType.emailAddress),
@@ -126,6 +158,9 @@ class _SignupScreenState extends State<SignupScreen> {
                 ),
                 RoundTextField(
                   onChanged: (p0) {
+                    setState(() {
+                      password = p0;
+                    });
                   },
                   hintText: "Password",
                   icon: "assets/icons/lock_icon.png",
@@ -179,7 +214,8 @@ class _SignupScreenState extends State<SignupScreen> {
                 RoundGradientButton(
                   title: "Register",
                   onPressed: () {
-                    Navigator.pushNamed(context, '/completeProfileScreen');
+                    // Navigator.pushNamed(context, '/completeProfileScreen');
+                    onRegisterButtonPressed();
                   },
                 ),
                 const SizedBox(
@@ -213,34 +249,46 @@ class _SignupScreenState extends State<SignupScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     GestureDetector(
-                      onTap: () {
-
-                      },
+                      onTap: () {},
                       child: Container(
                         width: 50,
                         height: 50,
                         alignment: Alignment.center,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(14),
-                          border: Border.all(color: AppColors.primaryColor1.withOpacity(0.5), width: 1, ),
+                          border: Border.all(
+                            color: AppColors.primaryColor1.withOpacity(0.5),
+                            width: 1,
+                          ),
                         ),
-                        child: Image.asset("assets/icons/google_icon.png",width: 20,height: 20,),
+                        child: Image.asset(
+                          "assets/icons/google_icon.png",
+                          width: 20,
+                          height: 20,
+                        ),
                       ),
                     ),
-                    const SizedBox(width: 30,),
+                    const SizedBox(
+                      width: 30,
+                    ),
                     GestureDetector(
-                      onTap: () {
-
-                      },
+                      onTap: () {},
                       child: Container(
                         width: 50,
                         height: 50,
                         alignment: Alignment.center,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(14),
-                          border: Border.all(color: AppColors.primaryColor1.withOpacity(0.5), width: 1, ),
+                          border: Border.all(
+                            color: AppColors.primaryColor1.withOpacity(0.5),
+                            width: 1,
+                          ),
                         ),
-                        child: Image.asset("assets/icons/facebook_icon.png",width: 20,height: 20,),
+                        child: Image.asset(
+                          "assets/icons/facebook_icon.png",
+                          width: 20,
+                          height: 20,
+                        ),
                       ),
                     ),
                   ],
